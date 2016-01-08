cd135b0b276605924b4e2cf7d58b94dfca70a655
Becky Smethurst <rebecca.smethurst@astro.ox.ac.uk>
2016-01-08 13:01:11 -0500

    update with fish eye function

--- marsfilter.py
+++ marsfilter.py
@@ -1,5 +1,6 @@
 import matplotlib.image as mpimg
 import numpy as np
+from skimage import transform, data, io
 
 def marsfilter(imfile):
 	img = mpimg.imread(imfile)
@@ -9,3 +10,19 @@ def marsfilter(imfile):
 	marsimg = img*mask 
 	imsavefile = 'mars_'+imfile
 	return mpimg.imsave(imsavefile, marsimg, dpi=100)
+	
+def fisheye(xy):
+    center = np.mean(xy, axis=0)
+    xc, yc = (xy - center).T
+
+    # Polar coordinates
+    r = np.sqrt(xc**2 + yc**2)
+    theta = np.arctan2(yc, xc)
+
+    r = 0.8 * np.exp(r**(1/2.1) / 1.8)
+
+    return np.column_stack((
+        r * np.cos(theta), r * np.sin(theta)
+        )) + center
+        
+#out = transform.warp(image, fisheye)
